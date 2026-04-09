190582a30700cbd983d75ef16df6eb11a1f1f880
Николай <usenkko.nikolay@gmail.com>
2026-04-09 14:01:04 +0300

    Add the IBankAccountRepository interface and its InMemoryBankAccountRepository implementation

---
+++ Repositories/IBankAccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using fourth_term_software_labs.Models;
+
+namespace fourth_term_software_labs.Repositories
+{
+    public interface IBankAccountRepository
+    {
+        IEnumerable<BankAccount> GetAll();
+        BankAccount? GetById(int id);
+        BankAccount? GetByAccountNumber(string accountNumber);
+        void Add(BankAccount account);
+        void Update(BankAccount account);
+        void Delete(int id);
+        IEnumerable<BankAccount> GetActiveAccounts();
+        IEnumerable<BankAccount> GetByCurrency(string currency);
+    }
+}
