c6703d8c19480cdb83af7d7e623a58dc31023987
Николай <usenkko.nikolay@gmail.com>
2026-04-09 12:41:06 +0300

    Add the IProductRepository interface and its InMemoryProductRepository implementation

---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using fourth_term_software_labs.Models;
+
+namespace fourth_term_software_labs.Repositories
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product GetById(int id);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int id);
+        IEnumerable<Product> GetByCategory(string category);
+        IEnumerable<Product> GetInStock();
+    }
+}
