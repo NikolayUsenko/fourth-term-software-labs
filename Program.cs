705f6ff06b456454ff99631c30be32d218b54681
Николай <usenkko.nikolay@gmail.com>
2026-04-21 21:02:55 +0300

    Register the context in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 using fourth_term_software_labs.Repositories;
+using Microsoft.EntityFrameworkCore;
+using fourth_term_software_labs.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+.LogTo(Console.WriteLine, LogLevel.Information)
+.EnableSensitiveDataLogging()
+);
 
 builder.Services.AddScoped<IProductRepository, InMemoryProductRepository>();
 
